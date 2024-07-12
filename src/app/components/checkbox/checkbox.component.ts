93c5f6d21171a58f3d5815d7ae55139179c90717
omateusamaral <mateus.amaral018@gmail.com>
2024-07-11 21:21:43 -0300

    refactor: adjustments to load checkbox

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Vehicle } from '../../../interfaces/vehicle.interface';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,22 +9,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './checkbox.component.html',
   styleUrl: './checkbox.component.css',
 })
-export class CheckboxComponent implements OnInit {
-  public checkboxes: string[] = [];
-
-  constructor(private http: HttpClient) {}
-  ngOnInit(): void {
-    this.loadCheckboxes();
-  }
-
-  loadCheckboxes(): void {
-    this.http.get<Vehicle[]>('assets/cars.json').subscribe(
-      (data) => {
-        this.checkboxes = [...new Set(data.map((x) => x.type))];
-      },
-      (error) => {
-        console.error('Error loading vehicles', error);
-      }
-    );
-  }
+export class CheckboxComponent {
+  @Input()
+  checkboxes!: string[];
 }
--- src/app/pages/search-car/search-car.component.ts
+++ src/app/pages/search-car/search-car.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 import { CollapseComponent, CheckboxComponent } from '../../components';
+import { HttpClient } from '@angular/common/http';
+import { Vehicle } from '../../../interfaces/vehicle.interface';
 
 @Component({
   selector: 'app-search-car',
@@ -8,4 +10,36 @@ import { CollapseComponent, CheckboxComponent } from '../../components';
   templateUrl: './search-car.component.html',
   styleUrl: './search-car.component.css',
 })
-export class SearchCarComponent {}
+export class SearchCarComponen implements OnInit, AfterContentInit {
+  public checkboxes: string[] = [];
+  public engineTypes: string[] = [];
+  public vehicles: Vehicle[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.loadVehicles();
+  }
+  ngAfterContentInit(): void {
+    this.loadCheckboxes();
+    this.loadEngineTypes();
+  }
+
+  loadVehicles(): void {
+    this.http.get<Vehicle[]>('assets/cars.json').subscribe(
+      (data) => {
+        this.vehicles = data;
+      },
+      (error) => {
+        console.error('Error loading vehicles', error);
+      }
+    );
+  }
+
+  loadCheckboxes(): void {
+    this.checkboxes = [...new Set(this.vehicles.map((x) => x.type))];
+  }
+
+  loadEngineTypes(): void {
+    this.engineTypes = [...new Set(this.vehicles.map((x) => x.engine))];
+  }
+}
